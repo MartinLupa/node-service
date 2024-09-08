907224ff51ffaa6440d04f7043d42eba88db9587
Martin Lupa <marlupa@dfds.com>
2024-09-08 22:06:42 +0200

    feat: conventional commit

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+};
+
+export default Configuration;
